69fb3447310b9617dd636d697b5b185fd1f5e2bc
lpongracz <48865727+pongraczl@users.noreply.github.com>
2022-01-28 16:01:53 +0100

    Test change 002

--- src/helpers/set-deployment-status.ts
+++ src/helpers/set-deployment-status.ts
@@ -11,7 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Test change 001
+// Test change 002
 
 import { DeploymentState } from '../types';
 import { GITHUB_OPTIONS } from '../constants';
